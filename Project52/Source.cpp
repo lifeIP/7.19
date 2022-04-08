4535757b629a92a321fa1d5c10a077c0eab3bf90
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-08 23:43:07 +0700

    last version

--- Project52/Source.cpp
+++ Project52/Source.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void findANDreplace(char* str, char* resault);
-void convert(float y, char* str);
+void convert(float y, float z, char* str);
 
 int main()
 {
@@ -17,11 +17,11 @@ int main()
 	cin >> y;
 	cout << "¬ведите z: ";
 	cin >> z;
-	char res[50];
-	convert(y, res);
+	char res[500];
+	convert(y, z, res);
 	cout << res << endl;
-	char* str1 = new char[100];
-	char* str2 = new char[110];
+	char* str1 = new char[2500];
+	char* str2 = new char[2500];
 	strcpy(str1, "9999999&999999");
 	findANDreplace(str1, str2);
 	cout << str1 << endl;
@@ -29,18 +29,18 @@ int main()
 	delete[]str1;
 	delete[]str2;
 }
-void convert(float y, char* str)
+void convert(float y,float z, char* str)
 {
-	double x = sin(3 * y) + 16.5;
-	sprintf(str, "%08.8f = sin(3* %08.8f)+16.5", x, y);
+	double x = 2 * log(pow(y,(3*z))) - 2*log(3*z);  //X= 2*log(Y,3*Z) Ц 2*ln(3*Z)
+	sprintf(str, "%14f = 2*log(%14f^(3*%14f)) Ц 2*ln(3*%14f)", x, y, z, z);
 }
 
 void findANDreplace(char* str_l, char* resault_l) {
-	memset(resault_l, 0, sizeof(char) * 100);
+	memset(resault_l, 0, sizeof(char) * 2500);
 
 	int adr = 0;
-	char start[250] = {};
-	char end[250] = {};
+	char start[2500] = {};
+	char end[2500] = {};
 
 	for (int i = 0; i < strlen(str_l); i++) {
 		start[i] = str_l[i];
@@ -51,12 +51,13 @@ void findANDreplace(char* str_l, char* resault_l) {
 	for (int i = adr+1, j = 0; i < strlen(str_l); i++, j++) {
 		end[j] = str_l[i];
 	}
-
-	char res[250] = {};
+	int count = 0;
+	char res[2500] = {};
 	char sas[] = "100500";
 	for (int i = 0; i < strlen(end); i++) {
 		if (end[i] >= '0' && end[i] <= '9') {		
 			strcat(res, sas);
+			count++;
 		}
 		else {
 			strncat(res, i + end, 1);
@@ -68,4 +69,5 @@ void findANDreplace(char* str_l, char* resault_l) {
 	{
 		resault_l[i] = start[i];
 	}
+	cout << "«амен: " << count << endl;
 }
